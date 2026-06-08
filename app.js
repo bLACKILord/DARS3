1e00a83dc90cf5c6d2df12de3de942abf70fb40e
Abdusamad Kilichev <159056921+bLACKILord@users.noreply.github.com>
2026-06-08 19:05:06 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,165 @@
+
+// let a = "Hello World";
+
+// function wordUpper(a){
+// console.log(a.toUpperCase());
+// }
+
+// wordUpper(a);
+
+
+
+//2
+
+// const wordsmall = function(a){
+//     console.log(a.toLowerCase());
+// }
+
+
+// wordsmall("HELLO WORLD");
+
+
+//3
+
+// let year = 2000
+
+// function oldyear(a, b = 2026) {
+//     return b - a;
+// }
+
+
+// console.log(oldyear(year));
+
+
+//4
+
+
+
+// function def(location = "Fargona", name) {
+//    console.log(`${name} ${location}da yashaydi`);
+
+// }
+
+
+// def("Toshkent", "Ali");
+
+
+
+//5
+
+
+// function search(a,b){
+//     if(a.includes(b)){
+//         console.log("true");
+//     } else {
+//         console.log("false");
+//     }
+// }
+
+
+// search("Hello World", "World");
+
+
+
+//6
+
+// function index(a){
+//     console.log(a.index(0));
+
+// }
+
+// index("Hello World")
+
+
+//7
+
+
+// function a(b) {
+//     return b.charCodeAt(2);
+// }
+
+// console.log(a("Hello World"));
+
+
+//8
+
+
+// function name(a,b){
+//     console.log(`Имя: ${a}, Фамилия: ${b}`);
+// }
+
+// name("Ali", "Vali");
+
+
+//9
+
+// function kalkulator(a, b, c) {
+//    switch (c) {
+//     case "+":
+//         return a + b;
+//     case "-":
+//         return a - b;
+//     case "*":
+//         return a * b;
+//     case "/":
+//         return a / b;
+//     default:
+//         return "Noto'g'ri amal";
+//         break;
+//    }
+// }
+
+
+// console.log(kalkulator(10, 5, "-"))
+
+
+
+//10
+
+// function text(a) {
+//     if (a.includes("@")) {
+//         console.log("Bu email formatida");
+//     }
+// }
+
+
+// text("jsnujin@gmail.com")
+
+
+
+//11
+
+// function search(text, word) {
+//     if (text.toLowerCase().includes(word.toLowerCase())) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+// }
+
+// search("Salom salom", "salom");
+
+//12
+
+
+
+// function word(word) {
+//     return word.charCodeAt(0) + word.charCodeAt(word.length);
+// }
+
+// console.log(word("salom"));
+
+
+//13
+
+
+// const startsVowel = (text) => {
+//     if (text.includes(text[0].toLowerCase())) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+// }
+
+// startsVowel("Apple");
+
